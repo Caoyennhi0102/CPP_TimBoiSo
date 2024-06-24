a6805f321ce8fb403b41ce3bc33fd19c19c7576c
Caoyennhi0102 <luuvinhphat1487@gmail.com>
2024-06-24 19:00:19 +0700

    Add project files.

---
+++ CPP_TimBoiSo.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+
+using namespace std;
+int main() {
+	cout << "Chương Trình Tính Bội Số";
+	int n;
+	cout << "Nhập số: ";
+	cin >> n;
+	for (int i = 1; i <= 10; i++) {
+		cout << " " << n * i;
+
+	}
+	
+	
+	
+	
+
+	return 0;
+}
